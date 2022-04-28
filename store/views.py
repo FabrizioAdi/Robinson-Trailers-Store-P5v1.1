ac0ce55091218e7bf8b102a677ff0f69dfdbd8b2
FabrizioAdi <adriannoszczyk@gmail.com>
2022-04-28 17:04:54 +0000

    Copy cart view to checkout view

--- store/views.py
+++ store/views.py
@@ -21,6 +21,15 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        
+    else:
+        order = {'get_cart_total':0, 'grt_cart_items':0}
+        items = []
+        
+    context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
 
