ba07efad9138692c6b6f09b00327f93ae4751653
FabrizioAdi <adriannoszczyk@gmail.com>
2022-04-28 11:38:20 +0000

    Add changes to view.py file

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from.models import *
-
 # Register your models here.
+from .models import *
+
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-admin.site.register(ShipingAddress)
-
-
-
-
-
-
+admin.site.register(ShippingAddress)
--- store/views.py
+++ store/views.py
@@ -3,17 +3,16 @@ from .models import *
 
 # Create your views here.
 def store(request):
-    products = Product.objects.all()
-    context = {'products':products}
-    return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
-    
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-
+        items = order.orderitem_set.all()
+        
     else:
         items = []
         
