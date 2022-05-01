db741d8c48eb606c2616c1e1b2faa26812255e44
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-01 17:07:00 +0000

    Update guest checkout in process order

--- store/views.py
+++ store/views.py
@@ -61,14 +61,45 @@ def updateItem(request):
         return JsonResponse('Item added to basket', safe=False)
 
 def processOrder(request):
+
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
 
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.object.get_or_create(customer=customer, commplete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
+
+    else:
+        print('User is not looged in')
+        print('COOKIES', request.COOKIES)
+
+        name = data['form']['name']
+        email = data['form']['email']
+        
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+        
+        customer, created = Customer.objects.get_or_create(
+				email=email,
+				)
+        customer.name = name
+        customer.save()
+        
+        order = Order.objects.create(
+			customer=customer,
+			complete=False,
+			)
+
+        for item in items:
+			product = Product.objects.get(id=item['id'])
+			orderItem = OrderItem.objects.create(
+				product=product,
+				order=order,
+				quantity=item['quantity'],
+			)
+            
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
     
     if total == order.get_cart_total:
         order.complete = True
@@ -78,14 +109,11 @@ def processOrder(request):
         ShipingAddress.objects.create(
             customer=customer,
             order=order,
-            address=data['shipping']['address'],
-            city=data['shipping']['city'],
-            state=data['shipping']['state'],
-            zipcode=data['shipping']['zipcode'],
+            address=data['shiping']['address'],
+            city=data['shiping']['city'],
+            state=data['shiping']['state'],
+            zipcode=data['shiping']['zipcode'],
         )
-    else:
-        print('User is not logged in')
-
 
     return JsonResponse('Payment submitted..', safe=False)
 
