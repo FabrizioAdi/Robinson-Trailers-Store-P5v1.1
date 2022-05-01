2f9febe8e743c355d683a57f64a0474d64acf9ac
FabrizioAdi <adriannoszczyk@gmail.com>
2022-05-01 16:01:18 +0000

    Add new file and import in views

--- store/views.py
+++ store/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-import datetime 
-
+import datetime
 from .models import *
+from .utils import cookieCart
 
 # Create your views here.
 def store(request):
